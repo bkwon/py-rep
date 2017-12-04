393990e37fe52edc8bb9e481211f12f15d810816
Bryant-Kwon <bryant.kwon@autotradergroup.com>
2017-12-03 18:05:05 -0800

    Added print statements to help my students understand the operations performed

--- str_slice.py
+++ str_slice.py
@@ -6,21 +6,32 @@ Test
 word = "everything"
 
 # Selecting substrings
-print(word[1:5])
+print("")
+print("Selecting Substrings:")
+print(word[1:5])  #gets all chars between 1-4 but not including 5
 print(word[5:9])
 
 # Open ended slices
+print("")
+print("Open Ended Slices:")
 print(word[5:])
 print(word[:4])
 
 # Using negative indices
+print("")
+print("Using negative indices:")
 print(word[-3:])
 print(word[2:-3])
 
 # Indexing past the end
+print("")
+print("Indexing past end:")
 print(word[8:20])
 print("$" + word[22:29] + "^")
 
 # Empty slices
+print("")
+print("Empty Slices:")
 print(word[6:6])
 print(word[4:2])
+print(type(word[6:6]))
