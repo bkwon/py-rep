275d16cbde69eaabcefdd678cd9872765d2e4ecf
Bryant-Kwon <bryant.kwon@autotradergroup.com>
2017-12-03 18:17:42 -0800

    initial commits

---
+++ test_string2int.py
@@ -0,0 +1,33 @@
+"""
+Template - Function that checks whether a string can be converted to an integer
+"""
+
+
+def make_int(int_string):
+    """
+    Given the string int_string, return the associated integer if all
+    digits are decimal digits.  Other return -1.
+    """
+
+    # enter code here
+    # pass
+    if int_string.isdigit():
+        return int(int_string)  #cast to int; otherwise would return string and include leading 0's in 2nd print stmt
+    else:
+        return -1
+
+
+# Tests
+
+print(make_int("123"))
+print(make_int("00123"))
+print(make_int("1.23"))
+print(make_int("-123"))
+
+
+# Output
+
+# 123
+# 123
+# -1
+# -1
