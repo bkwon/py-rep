ab84d298b26a4905a99d7724f6f384266922355e
Bryant-Kwon <bryant.kwon@autotradergroup.com>
2017-12-09 22:31:21 -0800

    initial commits

---
+++ strings_quiz.py
@@ -0,0 +1,16 @@
+
+statement = "Grail"
+
+print("Grail"[-1])
+print("Grail"[4])
+print("Grail"[len("Grail")-1])
+
+
+stmt2 = "Sir Robin"
+# which of the below will print "Sir"?
+print("Sir Robin"[:3])  # print anything up front to the 4th position or index==3 (0-3)
+print("Sir Robin"[1:3])
+print("Sir Robin"[0:3])
+print("Sir Robin"[1:4])
+
+
